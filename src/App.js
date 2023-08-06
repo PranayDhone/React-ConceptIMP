d00bdd73babbeafb7f69ccf5152809ca84125fbb
PranayDhone <pranayadhone18@gmail.com>
2023-08-06 12:45:42 +0530

    Adding new Movies intio the list FIRBASE

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useCallback, useEffect, useState } from "react";
 
 import MoviesList from "./components/MoviesList";
 import "./App.css";
+import AddMovie from "./components/AddMovie";
 
 function App() {
   const [movies, setMovies] = useState([]);
@@ -12,7 +13,9 @@ function App() {
     setIsLoading(true);
     setError(null);
     try {
-      const response = await fetch("https://swapi.dev/api/films"); //link agar galat hogi toh error show karne ke liye something went wrong useState use kiya hai
+      const response = await fetch(
+        "https://react-http-c1eea-default-rtdb.firebaseio.com/movies.json"
+      ); //link agar galat hogi toh error show karne ke liye something went wrong useState use kiya hai
       if (!response.ok) {
         throw new Error("Something went wrong!");
       }
@@ -38,8 +41,15 @@ function App() {
     fetchMoviesHandler();
   }, [fetchMoviesHandler]);
 
+  function addMovieHandler(movie) {
+    console.log(movie);
+  }
+
   return (
     <React.Fragment>
+      <section>
+        <AddMovie onAddMovie={addMovieHandler}></AddMovie>
+      </section>
       <section>
         <button onClick={fetchMoviesHandler}>Fetch Movies</button>
       </section>
